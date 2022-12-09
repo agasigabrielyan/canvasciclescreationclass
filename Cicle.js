fd3dd369ef3acca4c5fa96571c19a142271f7091
agasi <agasi79@list.ru>
2022-12-09 11:13:44 +0300

    Initial commit

---
+++ Cicle.js
@@ -0,0 +1,16 @@
+class Cicle {
+    // создадим метод конструктор
+    constructor(xpos, ypos, radius, color) {
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    // создадим метод, который рисует
+    draw( context ) {
+        context.beginPath();
+        context.arc( this.xpos, this.ypos, this.radius, 0, Math.PI*2, false );
+    }
+}
+
---
+++ script.js
@@ -0,0 +1,13 @@
+let canvas = document.getElementById('map');
+let context = canvas.getContext("2d");
+
+let windowHeight = window.innerHeight;
+let windowWidth = window.innerWidth;
+
+canvas.width = windowWidth;
+canvas.height = windowHeight;
+
+canvas.style.background = "yellow";
+
+
+
